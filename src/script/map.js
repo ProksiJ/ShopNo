796639af0a34aa72efdc425d55537952d0af0a3c
ProksiJ <tomasheva.ksenia@yandex.ru>
2022-09-27 15:02:02 +0300

    fix map.js

--- src/script/map.js
+++ src/script/map.js
@@ -1,13 +1,12 @@
 function initMap() {
-    const location = { lat: 55.1930197, lng: 30.2070437 };
+    const position = { lat: 55.1930197, lng: 30.2070437 };
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 8,
-      center: location,
+      center: position,
     });
     const marker = new google.maps.Marker({
-      position: location,
+      position,
       map,
     });
   }
   
-  window.initMap = initMap;
